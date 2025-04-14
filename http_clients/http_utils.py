706bc40a58c427ea38fabe9a93373e4af94e842c
mido <midohany910@gmail.com>
2025-04-14 18:13:03 +0200

    Implement fully customizable retry_on_exception

--- http_clients/http_utils.py
+++ http_clients/http_utils.py
@@ -1,31 +1,44 @@
 import time
 import functools
-import logging
 from requests.exceptions import RequestException
+import logging
 
-logging.basicConfig(level=logging.INFO)
 
 def retry_on_exception(
     exceptions=(RequestException,),
     max_retries=3,
     initial_delay=1,
     backoff_factor=2,
-    max_delay=30
+    max_delay=30,
+    on_retry=None,
+    on_success=None,
+    on_failure=None,
+    logger=logging,
 ):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             retries = 0
             delay = initial_delay
+            
             while True:
                 try:
-                    return func(*args, **kwargs)
+                    result = func(*args, **kwargs)
+                    if on_success:
+                        on_success(result)
+                    return result
+                
                 except exceptions as e:
                     retries += 1
                     if retries > max_retries:
                         logging.error(f"Max retries exceeded: {e}")
+                        if on_failure:
+                            on_failure(e)
                         return
-                    logging.info(f"Retrying ({retries}/{max_retries}) in {delay}s due to: {e}")
+                    
+                    logging.warning(f"Retrying ({retries}/{max_retries}) in {delay}s due to: {e}")
+                    if on_retry:
+                        on_retry(e)
                     time.sleep(min(delay, max_delay))
                     delay *= backoff_factor
         return wrapper
@@ -33,11 +46,21 @@ def retry_on_exception(
 
 if __name__=="__main__":
     import requests
+    from requests import Response
     
-    @retry_on_exception()
+    logging.basicConfig(level=logging.INFO)
+    
+    def on_retry(e):
+        logging.info(f"Retrying due to: {e}")
+    def on_success(r: Response):
+        logging.info("Request succeeded")
+        if r.ok:
+            logging.info(r.json())
+    def on_failure(e):
+        logging.info(f"Request failed: {e}")
+    
+    @retry_on_exception(on_retry=on_retry, on_success=on_success, on_failure=on_failure)
     def send_request(*args, **kwargs):
         return requests.request(*args, **kwargs)
     
-    r = send_request('GET', 'https://httpbin.org/ip', timeout=3)
-    if r.ok:
-        print(r.json())
+    r = send_request('GET', 'https://httpbin.org/ip', timeout=5)
